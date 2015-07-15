1762792857b73a194042dcc9cde61aed45fd1e27
da99 <i-hate-spam-1234567@mailinator.com>
2015-07-15 13:33:43 -0500

    Improved: design. Just one function to define an entire attr.

--- applet.js
+++ applet.js
@@ -12,23 +12,6 @@ var Applet = {
 
   attrs : {
     show_if : function (env) {
-      env.$.hide();
-
-      // === Add to dom:
-      if (env.is_parent)
-        env.script.after(env.$);
-
-      var val = env.val;
-      var id = Applet.id(env.$);
-      env.append(function (e) {
-        if (!_.has(e.data, val))
-          return;
-
-        if (e.data[val])
-          $('#' + id).show();
-        else
-          $('#' + id).hide();
-      });
     },
 
     hide_if : function (env) {
@@ -85,7 +68,10 @@ var Applet = {
 
   compile : function () {
 
-    var attr_names = _.keys(Applet.attrs);
+    var attr_names = _.compact(_.map(Applet.stacks.funcs, function (f) {
+      f({name: 'find_attr'});
+    }));
+
     var any_with_attrs   = _.map(attr_names, function (name) {
       return '*[' + name + ']';
     }).join(',');
@@ -100,15 +86,18 @@ var Applet = {
           if (!dom.attr(attr))
             return;
           var parent = dom.parent();
-          Applet.attrs[attr]({
-            script     : script,
-            raw_script : raw_script,
-            raw        : raw,
-            $          : dom,
-            val        : dom.attr(attr),
-            is_child   : parent.length > 0,
-            is_parent  : parent.length === 0,
-            append     : Applet.append
+          _.each(Applet.stacks.funcs, function (f) {
+            f({
+              name       : 'raw_node',
+              script     : script,
+              raw_script : raw_script,
+              raw        : raw,
+              $          : dom,
+              val        : dom.attr(attr),
+              is_child   : parent.length > 0,
+              is_parent  : parent.length === 0,
+              append     : Applet.append
+            });
           });
         }); // === each attr_names
       });
@@ -116,54 +105,6 @@ var Applet = {
     }); // === each script applet
 
   }, // === compile
-
-  compile_script_tag : function (raw_script) {
-
-    var script    = $(raw_script);
-    var script_id = Applet.id(raw_script);
-    $(script.html()).each(function (i, raw_child) {
-
-      var child = $(raw_child);
-      _.each(_.keys(Applet.attrs), function (name) {
-
-        var val = child.attr(name);
-        if (!val)
-          return;
-
-        Applet.nodes.push({
-          id:     Applet.id(child, script_id),
-          action: name,
-          value:  val
-        });
-
-        child.removeAttr(name);
-
-        switch (name) {
-
-          case 'show_if':
-            if (Applet.bool( val ))
-              child.show();
-            else
-              child.hide();
-            break;
-
-          case 'hide_if':
-            if (Applet.bool( val ))
-              child.hide();
-            else
-              child.show();
-            break;
-
-          default:
-            Applet.log('I do not know what to do on: ' + name);
-        } // === switch
-
-      }); // === each Applet action
-
-      script.after(child);
-    });
-
-  }, // === compile_script
 
   run: function (raw_data) {
     // === first data
@@ -183,8 +124,45 @@ var Applet = {
   }
 };
 
+Applet.run(function (e) {
+
+  switch (e.name) {
+
+    case 'find_attr':
+      return 'show_if';
+
+    case 'raw_node':
+      e.$.hide();
+
+      // === Add to dom:
+      if (e.is_parent)
+        e.script.after(e.$);
+      return;
+
+    case 'attr':
+      if (!e.show_if)
+        return;
+
+      if (!_.has(e.data, e.show_if))
+        return;
+
+      if (e.data[e.show_if])
+        $('#' + e.id).show();
+      else
+        $('#' + e.id).hide();
+      return;
+
+  } // === switch e.name
+
+}); // === show_if
+
 if (window.$)
   $(function () {
     Applet.compile();
   });
+
+
+
+
+
 
