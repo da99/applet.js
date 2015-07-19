24a810e4d9554505087e2fab5d7621b4a8b54013
da99 <i-hate-spam-1234567@mailinator.com>
2015-07-19 11:21:08 -0500

    Dev. checkpoint.

--- specs.js
+++ specs.js
@@ -2,5 +2,13 @@
 $(function () {
   Applet.run({name: 'compile scripts'});
   Applet.run({"logged_in?": false});
+
+  Applet.run({num: {number: 1}});
+  Applet.run({num: {number: 2}});
+  Applet.run({num: {number: 3}});
+
+  Applet.run({num_name: {word: 'three'}});
+  Applet.run({num_name: {word: 'two'}});
+  Applet.run({num_name: {word: 'one'}});
 });
 
