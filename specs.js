975f2294072358c9f3cde611ebbe7014ad030c08
da99 <i-hate-spam-1234567@mailinator.com>
2015-07-21 15:58:22 -0500

    Specs for: .custom_attrs

--- specs.js
+++ specs.js
@@ -11,10 +11,6 @@ $(function () {
   Applet.run({num_name: {word: 'two'}});
   Applet.run({num_name: {word: 'one'}});
 
-  console.log(
-    Applet.attrs($('<div id="000" show_if="happy?"></div>')[0])
-  )
-
 });
 
 
@@ -33,3 +29,26 @@ describe('attrs:', function () {
   }); // === it ignores custom attributes
 
 }); // === describe attrs =================
+
+
+describe('custom_attrs:', function () {
+
+  beforeEach(Applet.reset);
+
+  it('returns custom attrs of node', function () {
+    Applet.run(function (e) { return 'hide_if'; });
+
+    var result = Applet.custom_attrs($('<div show_if="sad?" hide_if="happy?"></div>')[0]);
+    expect(result).toEqual(
+      {show_if: "sad?", hide_if: 'happy?'}
+    );
+  }); // === it returns custom attrs of node
+
+  it('ignores regular attributes', function () {
+    var result = Applet.custom_attrs($('<div show_if="mellow?" id="222"></div>')[0]);
+    expect(result).toEqual(
+      {show_if: "mellow?"}
+    );
+  }); // === it ignores regular attributes
+
+}); // === describe custom_attrs =================
