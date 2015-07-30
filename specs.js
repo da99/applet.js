6b9865012b08abfa08d810c6e2d776ef2b227db5
da99 <i-hate-spam-1234567@mailinator.com>
2015-07-30 14:20:05 -0500

    Imported: code from megauni.js repo

--- specs.js
+++ specs.js
@@ -2,6 +2,8 @@
 /* jshint undef: true, unused: true */
 /* global Applet, describe, it, expect, beforeEach */
 
+var app;
+
 describe('Applet:', function () {
 
   beforeEach(function () {
@@ -205,4 +207,176 @@ describe('Applet:', function () {
     }); // === it does not return nested matching descendants if ancestor matches selector
 
   }); // === describe closest =================
+
+  describe('run:', function () {
+
+    it('inserts contents before SCRIPT tag', function () {
+      $('#THE_STAGE')
+      .html('<script type="text/applet"><div id="target" show_if="logged_in?">logged</div></script>');
+      app = new Applet();
+      expect(
+        $($('#THE_STAGE').children().first()).attr('id')
+      ).toEqual('target');
+    }); // === it inserts contents before SCRIPT tag
+
+  }); // === describe run =================
+
+  describe('show_if:', function () {
+
+    it('sets node to display=none by default', function () {
+      $('#THE_STAGE')
+      .html('<script type="text/applet"><div id="target" show_if="logged_in?">logged</div></script>');
+      app = new Applet();
+      expect(
+        $('#target').css('display')
+      ).toEqual('none');
+    }); // === it sets node to display=none by default
+
+    it('makes node visible if data has a truthy kv', function () {
+      $('#THE_STAGE')
+      .html('<script type="text/applet"><div id="target" show_if="logged_in?">logged</div></script>');
+
+      app = new Applet();
+      app.run('data', {'logged_in?': true});
+
+      expect(
+        $('#target').css('display')
+      ).toEqual('block');
+    }); // === it makes node visible if data has a truthy kv
+
+  }); // === describe show_if =================
+
+  describe('template:', function () {
+
+    it('does not render by default', function () {
+      $('#THE_STAGE').html(
+        '<script type="text/applet"><div template="num">{{num.word}} {{num.num}}</div></script>'
+      );
+
+      app = new Applet();
+
+      expect(
+        _.map($('#THE_STAGE').children(), function (n) {
+          return $(n).attr('type');
+        })
+      ).toEqual(['text/applet_placeholder', 'text/applet']);
+    }); // === it does not render by default
+
+    it('renders elements on top of where it is found', function () {
+      $('#THE_STAGE').html(
+        '<script type="text/applet"><div template="num">{{num.word}} {{num.num}}</div></script>'
+      );
+
+      app = new Applet();
+      app
+      .run('compile scripts')
+      .run('data', {num: {word: 'one', num: 1}})
+      ;
+
+      expect(
+        $('#THE_STAGE').children().first().prop('outerHTML')
+      ).toEqual('<div>one 1</div>');
+    }); // === it renders elements on top of where it is found
+
+    it('replaces elements, including text nodes', function () {
+      $('#THE_STAGE').html(
+        '<script type="text/applet"><div template="num">{{num.word}} {{num.num}}</div></script>'
+      );
+
+      app = new Applet();
+      app
+      .run('compile scripts')
+      .run('data', {num: {word: 'one', num: Date.now().toString()}})
+      .run('data', {num: {word: 'two', num: 2}})
+      ;
+
+
+      expect(
+        $('#THE_STAGE').text()
+      ).toEqual('two 2');
+    }); // === it replaces elements, including text nodes
+
+    it('appends rendered template above w/ option: top', function () {
+      $('#THE_STAGE').html(
+        '<script type="text/applet"><div template="num top">{{num.word}} {{num.num}}</div></script>'
+      );
+
+      app = new Applet();
+      app
+      .run('compile scripts')
+      .run('data', {num: {word: 'one', num: 1}})
+      .run('data', {num: {word: 'two', num: 2}})
+      ;
+
+      expect(
+        $('#THE_STAGE').text()
+      ).toEqual('two 2one 1');
+    }); // === it appends rendered template above w/ option: top
+
+    it('appends rendered template below w/ option: bottom', function () {
+      $('#THE_STAGE').html(
+        '<script type="text/applet"><div template="num bottom">{{num.word}} {{num.num}}</div></script>'
+      );
+
+      app = new Applet();
+
+      app
+      .run('compile scripts')
+      .run('data', {num: {word: 'one', num: 1}})
+      .run('data', {num: {word: 'two', num: 2}})
+      .run('data', {num: {word: 'three', num: 3}})
+      ;
+
+      expect(
+        $('#THE_STAGE').text()
+      ).toEqual('one 1two 2three 3');
+    }); // === it appends rendered template above w/ option: bottom
+
+    it('renders template w/ attr functionality', function () {
+      $('#THE_STAGE').html(
+        '<script type="text/applet"><div template="num" id="target"><span show_if="show_num?">{{num.word}}</span></div></script>'
+      );
+
+      app = new Applet();
+      app
+      .run('compile scripts')
+      .run('data', {'show_num?': true, num: {word: 'number'}})
+      ;
+
+      expect(
+        $('#target span').css('display')
+      ).toEqual('inline');
+
+      app.run('data', {'show_num?' : false});
+
+      expect(
+        $('#target span').css('display')
+      ).toEqual('none');
+
+    }); // === it renders template w/ attr functionality
+
+  }); // === describe template =================
+
+  describe('forms:', function () {
+
+    it('adds handlers to buttons', function () {
+      $('#THE_STAGE').html(
+        '<form id="target"><input type="hidden" name="hello" value="goodbye" /><button class="submit">SUBMIT</button></form>'
+      );
+      app = new Applet();
+
+      var result = null;
+      app.push(function (o) {
+        if (o.name !== 'form submit')
+          return;
+        result = o.data.hello;
+        o['submit?'] = false;
+      });
+
+      $('#THE_STAGE button.submit').click();
+      expect(result).toEqual('goodbye');
+    }); // === it adds handlers to buttons
+  }); // === describe forms =================
+
+
 }); // === describe Applet =================
